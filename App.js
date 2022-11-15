20fd22ea7e14b114e449026b24a9465c20a76821
Oskar Zdziarski <oskarzdz@gmail.com>
2022-11-14 19:06:51 -0800

    add nativewind

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [val, setVal] = useState(0);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-zinc-900">
       <StatusBar style="auto" />
+      <View className="flex flex-row ">
+        <Button title='+' onPress={() => setVal(val + 1)} />
+        <Button title='-' onPress={() => setVal(val - 1)} />
+      </View>
+      <Text className="text-white">Increment me: {val}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
