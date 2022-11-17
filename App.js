f2ac778698f4a6c92299879f2c75a0b5088d0b71
Oskar Zdziarski <oskarzdz@gmail.com>
2022-11-17 13:25:41 -0800

    create custom button

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, Button } from 'react-native';
+import { Text, View } from 'react-native';
 import { useState } from 'react';
+import ZButton from './src/components/ZButton';
 
 export default function App() {
   const [val, setVal] = useState(0);
   return (
-    <View className="flex-1 items-center justify-center bg-zinc-900">
+    <View className="flex-1 items-center justify-center bg-zinc-900 gap-4">
       <StatusBar style="auto" />
-      <View className="flex flex-row ">
-        <Button title='+' onPress={() => setVal(val + 1)} />
-        <Button title='-' onPress={() => setVal(val - 1)} />
+      <View className="flex flex-row gap-1">
+        <ZButton title='+' onPress={() => setVal(val + 1)} />
+        <ZButton title='-' onPress={() => setVal(val - 1)} />
+        <ZButton title="reset" onPress={() => setVal(0)} />
       </View>
-      <Text className="text-white">Increment me: {val}</Text>
+      <Text className="text-white">{val}</Text>
     </View>
   );
 }
